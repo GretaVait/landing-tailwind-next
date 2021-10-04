2be616d8069e783bfc58d924534814ddf34f5d9f
GretaVait895 <gretavait@gmail.com>
2021-10-04 15:50:15 +0300

    tailwind init

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Landing</title>
+        <meta name="description" content="Landing Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        Hello
+      </main>
+    </div>
+  )
+}
