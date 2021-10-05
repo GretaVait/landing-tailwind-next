10e46589a5c62aa83735a049bf5c7f8c79a4e939
GretaVait895 <gretavait@gmail.com>
2021-10-05 14:41:15 +0300

    select plan removed part of content

--- pages/create-plan/index.jsx
+++ pages/create-plan/index.jsx
@@ -8,7 +8,7 @@ import IntroImg from '/public/images/1.jpg'
 import Intro from '../../sections/Intro'
 import SelectPlan from '../../sections/SelectPlan'
 
-export default function CreatePlan({  }) {
+export default function CreatePlan() {
   return (
     <div>
       <Head>
@@ -25,7 +25,7 @@ export default function CreatePlan({  }) {
           description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." 
         />
 
-        {/* <SelectPlan /> */}
+        <SelectPlan />
 
       </main>
     </div>
