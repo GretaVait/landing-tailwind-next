535ae396efa7f51a800cfd4b16089b10426e5415
GretaVait895 <gretavait@gmail.com>
2022-01-03 10:10:15 +0200

    pwa files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const withPWA = require("next-pwa")
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'i.ibb.co']
-  }
-}
+  },
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development"
+  },
+})
